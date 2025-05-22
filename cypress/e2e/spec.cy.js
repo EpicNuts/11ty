34d99623d26acaff77cbcb6f8fd7bfad72f7fd03
Ben Luck <benjamonstyles@protonmail.com>
2025-05-22 11:28:10 +0200

    added vercel authentication

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,7 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('/')
+  
+  it('passes', () => {  
+    cy.visit('/')    
     cy.get('body > :nth-child(1)').should('have.text', 'Hi there')
   })
 })
