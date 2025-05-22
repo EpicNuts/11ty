eccb31090fa826dbcc2514abf767945da93128b4
Ben Luck <benjamonstyles@protonmail.com>
2025-05-22 11:34:45 +0200

    wip

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,13 @@
 describe('template spec', () => {
   
-  it('passes', () => {  
-    cy.visit('/')    
+  it('passes', () => {
+    const secret = Cypress.env('VERCEL_AUTOMATION_BYPASS_SECRET')
+    cy.log(`VERCEL_AUTOMATION_BYPASS_SECRET: ${secret}`) // This prints to the Cypress log
+
+    
+    cy.visit('/')
+    cy.wait(10000)
+    
     cy.get('body > :nth-child(1)').should('have.text', 'Hi there')
   })
 })
